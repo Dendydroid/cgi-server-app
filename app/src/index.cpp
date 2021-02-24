dd6a6fb399cad6282a4264662b9852999c4a0e69
dendydroid <galatsiuk.taras@gmail.com>
2021-02-25 01:43:51 +0200

    structure example

--- app/src/index.cpp
+++ app/src/index.cpp
@@ -1,16 +1,12 @@
-#include <iostream>
+#include "core/output.h"
+
 using namespace std;
 
 int main () {
-   cout << "Content-type:text/html\r\n\r\n";
-   cout << "<html>\n";
-   cout << "<head>\n";
-   cout << "<title>Hello World - First CGI Program</title>\n";
-   cout << "</head>\n";
-   cout << "<body>\n";
-   cout << "<h2>Hello World! This32 is my first CGI program</h2>\n";
-   cout << "</body>\n";
-   cout << "</html>\n";
+
+   OutputManager *a = new OutputManager();
+
+   a->hello();
    
    return 0;
 }
